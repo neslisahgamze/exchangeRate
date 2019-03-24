eab3fb1042d251914905fbde5f0912da5000c81a
Neslişah Gamze Hayırlıoğlu <neslisah@artistanbul.io>
2019-03-24 12:18:53 +0300

    Update exchange rate controller and view

--- src/Controller/ExchangeRateController.php
+++ src/Controller/ExchangeRateController.php
@@ -10,10 +10,27 @@ class ExchangeRateController extends AbstractController
     /**
      * @Route("/exchange/rate", name="exchange_rate")
      */
+
+     /**
+     * @var ExchangeRateRepository
+     */
+    private $repository;
+    /**
+     * ExchangeRateController constructor.
+     * @param ExchangeRateRepository $repositorys
+     */
+    public function __construct(ExchangeRateRepository $repository)
+    {
+        $this->repository = $repository;
+
+    }
+
     public function index()
     {
+    	$rate = $this->repository->getMinimum();
+        $rate =compact('rate');
         return $this->render('exchange_rate/index.html.twig', [
-            'controller_name' => 'ExchangeRateController',
+            'rate' => $rate,
         ]);
     }
 }
